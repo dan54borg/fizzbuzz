2cb7c08a86c8a96477a1f2f4044b898b08486675
Dan Borg <dan54borg@gmail.com>
2026-04-20 15:17:18 +0100

    added fizzbuzz test

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,6 +2,10 @@ from main import fizzbuzz
 
 def test_div_by_3():
     assert fizzbuzz(3) == 'Fizz'
+    assert fizzbuzz(6) == 'Fizz'
 def test_div_by_5():
     assert fizzbuzz(5) == 'Buzz'
-    
+    assert fizzbuzz(10) == 'Buzz'
+def test_div_by_3_and_5():
+    assert fizzbuzz(15) == 'FizzBuzz'
+    assert fizzbuzz(30) == 'FizzBuzz'
