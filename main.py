111c6c6d916467ee8e3baf96c56967e708df2205
Dan Borg <dan54borg@gmail.com>
2026-04-20 15:20:06 +0100

    updated main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def fizzbuzz(number):
-    if number % 3 == 0:
+    if number % 15 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
         return "Fizz"
     elif number % 5 == 0:
         return "Buzz"
-    elif number % 15 == 0:
-        return "FizzBuzz"
     return number
